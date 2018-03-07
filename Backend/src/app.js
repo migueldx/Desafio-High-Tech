16ce0211d2cd1ac3fc449e3273a81c42d4745645
MIGUEL DIAS XIMENES <migueldiasximenes@gmail.com>
2018-03-07 12:10:07 -0400

    Criação das rotas post, put, delete

---
+++ Backend/src/app.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.2"
+    });
+});
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+app.use('/', route);
+app.use('/aluno', create);
+app.use('/aluno', put);
+app.use('/aluno', del);
+
+module.exports = app;
