b38f1fdfb04a664896d4a25c7ec0347afe2e458f
MIGUEL DIAS XIMENES <migueldiasximenes@gmail.com>
2018-03-09 08:29:49 -0400

    Modificação em aluno

--- Backend/src/model/aluno.js
+++ Backend/src/model/aluno.js
@@ -4,23 +4,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    ra: {
-        type: Number,
-        required: true,
-        unique: true,
-        index: true
-    },
+    // ra: {
+    //     type: Number,
+    //     required: true,
+    //     unique: true,
+    //     index: true
+    // },
     nome:{
         type: String,
         required: true,
         trim: true
     },
-    turmas: [{
-         turma: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Turma'
-        },
-    }] 
 });
 
 module.exports = mongoose.model('Aluno', schema);
