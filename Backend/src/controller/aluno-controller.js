a850d18ab073073f9ec03946d30dff2be7d644d5
MIGUEL DIAS XIMENES <migueldiasximenes@gmail.com>
2018-03-08 21:21:36 -0400

    Atualizando aluno-repository e aluno-controller com async e await

--- Backend/src/controller/aluno-controller.js
+++ Backend/src/controller/aluno-controller.js
@@ -2,99 +2,81 @@
 
 const mongoose = require('mongoose');
 const Aluno = mongoose.model('Aluno');
+const repository = require('../repository/aluno-repository');
 
-exports.post = (req, res, next) => {
-    var aluno = new Aluno(req.body);
-    aluno
-        .save()
-        .then(x => {
-            res.status(201).send({ message: 'Aluno cadastrado com sucesso!' });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao cadastrar Aluno!',
-                data: e
-            });
-
+exports.post = async (req, res, next) => {
+    try {
+        await repository.create(req.body)
+        res.status(201).send({ message: 'Aluno cadastrado com sucesso!' });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
         });
+    }
+}
 
-};
+exports.get = async (req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
 
-exports.get = (req, res, next) => {
-    Aluno
-        .find({}, 'ra nome')
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
+exports.getByRa = async (req, res, next) => {
+    try {
+        var data = await repository.getByRa(req.params.ra);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
 
-        });;
-};
+exports.getById = async (req, res, next) => {
+    try {
+        var data = await repository.getById(req.params.id);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
 
-exports.getByRa = (req, res, next) => {
-    Aluno
-        .findOne({
-            ra: req.params.ra
-        }, 'ra nome')
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
-
-        });;
-};
-
-exports.getById = (req, res, next) => {
-    Aluno
-        .findById(req.params.id)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
-
-        });;
-};
-
-exports.getByTag = (req, res, next) => {
-    Product
-        .find({
-            tags: req.params.tag,
-            active: true
-        }, 'title description price slug tags')
+exports.getByTag = async (req, res, next) => {
+    repository
+        .getByTag(req.params.tag)
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
             res.status(400).send(e);
         });
-};
+}
 
-exports.put = (req, res, next) => {
-     Aluno
-        .findByIdAndUpdate (req.params.id, {
-            $set:{
-                nome: req.body.nome
-            }
-        }).then(x => {
-            res.status(201).send({message: 'Aluno atualizado com sucesso!'});
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao atualizar Aluno',
-                data: e
-            });
+exports.put = async (req, res, next) => {
+    try {
+        await repository.update(req.params.id, req.body)
+        res.status(201).send({ message: 'Aluno atualizado com sucesso!' });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
 
-        });;
-};
-
-exports.delete = (req, res, next) => {
-     Aluno
-        .findOneAndRemove (req.body.id)
-        .then(x => {
-            res.status(200).send({message: 'Aluno removido com sucesso!'});
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao remover Aluno',
-                data: e
-            });
-
-        });;
-};
+exports.delete = async (req, res, next) => {
+    try {
+        await repository.delete(req.body.id)
+        res.status(200).send({ message: 'Aluno removido com sucesso!' });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+        });
+    }
+}
 
---
+++ Backend/src/repository/aluno-repository.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Aluno = mongoose.model('Aluno');
+
+exports.create = async (data) => {
+    var aluno = new Aluno(data);
+    await aluno.save();
+}
+
+exports.get = async () => {
+    const res = await Aluno
+        .find({}, 'ra nome');
+    return res;
+}
+
+exports.getByRa = async (ra) => {
+    const res = await Aluno
+        .findOne({
+            ra: ra
+        }, 'ra nome');
+    return res;
+}
+
+exports.getById = async (id) => {
+    const res = await Aluno
+        .findById(id);
+    return res;
+}
+
+exports.getByTag = async (tag) => {
+    const res = await Product
+        .find({
+            tags: tag,
+            active: true
+        }, 'title description price slug tags');
+    return res;
+}
+
+exports.update = async (id, data) => {
+    await Aluno
+        .findByIdAndUpdate(id, {
+            $set: {
+                nome: data.nome
+            }
+        });
+}
+
+exports.delete = async (id) => {
+    await Aluno
+        .findOneAndRemove(id);
+
+}
