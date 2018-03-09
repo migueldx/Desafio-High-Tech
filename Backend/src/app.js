e9865d3a455295793ce68ea16e75c47789a24871
MIGUEL DIAS XIMENES <migueldiasximenes@gmail.com>
2018-03-09 09:54:09 -0400

    Configuração do Cross Origin no app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -28,6 +28,14 @@ const aulaRoute = require('./routes/aula-route');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//Habilita o CORS
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', indexRoute);
 app.use('/aluno', alunoRoute);
 app.use('/turma', turmaRoute);
--- Backend/src/model/aluno.js
+++ Backend/src/model/aluno.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    // ra: {
-    //     type: Number,
-    //     required: true,
-    //     unique: true,
-    //     index: true
-    // },
+   ra: {
+       type: Number,
+         required: true,
+         unique: true,
+         index: true
+     },
     nome:{
         type: String,
         required: true,
